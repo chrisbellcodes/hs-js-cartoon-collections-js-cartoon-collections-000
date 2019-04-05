ec91a5a00997599564e310433111496d899856ca
Chris Bell <cdangelobell@gmail.com>
2019-04-05 15:59:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 function dwarfRollCall(dwarves) {
+  var dwarveCount = '';
+  for(let i = 0; i < dwarves.length; i++) {
+    dwarveCount += `${i + 1}. ${dwarves[i]} `
+  }
+  return dwarveCount;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var allCapsSummon = [];
+  for(let i = 0; i < planeteerCalls.length; i++) {
+    allCapsSummon.push(`${planeteerCalls[i].toUpperCase()}!`);
+  }
+  return allCapsSummon;
 }
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length <= 4) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
 
 function findTheCheese (foods) {
+var noCheese = [];
+for (let i = 0; i < foods.length; i++) {
+    switch(foods[i]) {
+      case 'cheddar':
+        return 'cheddar';
+      case 'gouda':
+        return 'gouda';
+      case 'camembert':
+        return 'camembert';
+    }
+    noCheese.push(foods[i]);
+}
+if (noCheese.length === foods.length) {
+  return 'no cheese!';
+}
 }
